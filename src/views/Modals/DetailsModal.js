18ef5090d1dc9fbc41e89e0b6084211b9e15d570
Abdelrahman Reda <boodycat09@gmail.com>
2023-06-25 15:53:02 +0300

    fix-modal-responsive

--- src/views/Modals/DetailsModal.js
+++ src/views/Modals/DetailsModal.js
@@ -1,5 +1,13 @@
 import FModal from '../../components/FModal'
-import { Area, AreaChart, CartesianGrid, Tooltip, XAxis, YAxis } from 'recharts'
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  Tooltip,
+  XAxis,
+  YAxis,
+  ResponsiveContainer,
+} from 'recharts'
 import FButton from '../../components/FButton'
 import FProductTag from '../../components/FProductTag'
 import { useEffect, useState } from 'react'
@@ -525,47 +533,62 @@ const DetailsModal = ({ isOpen, setIsOpen, merchantData }) => {
               <ESpinner isVisible={true} />
             ) : (
               <>
-                <AreaChart
-                  width={width > 768 ? 900 : 320}
-                  height={250}
-                  data={graphDataPlot}>
-                  <defs>
-                    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                      <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-                      <stop offset="80%" stopColor="#8884d8" stopOpacity={0} />
-                    </linearGradient>
-                    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                      <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-                      <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-                    </linearGradient>
-                  </defs>
-                  <XAxis dataKey="name" />
-                  <YAxis />
+                <ResponsiveContainer width={'100%'} height={250}>
+                  <AreaChart height={250} data={graphDataPlot}>
+                    <defs>
+                      <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                        <stop
+                          offset="5%"
+                          stopColor="#8884d8"
+                          stopOpacity={0.8}
+                        />
+                        <stop
+                          offset="80%"
+                          stopColor="#8884d8"
+                          stopOpacity={0}
+                        />
+                      </linearGradient>
+                      <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                        <stop
+                          offset="5%"
+                          stopColor="#82ca9d"
+                          stopOpacity={0.8}
+                        />
+                        <stop
+                          offset="95%"
+                          stopColor="#82ca9d"
+                          stopOpacity={0}
+                        />
+                      </linearGradient>
+                    </defs>
+                    <XAxis dataKey="name" />
+                    <YAxis />
 
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <Tooltip />
-                  <Area
-                    type="monotone"
-                    dataKey="achieved"
-                    stroke="#8884d8"
-                    fillOpacity={1}
-                    fill="url(#colorUv)"
-                  />
-                  <Area
-                    type="monotone"
-                    dataKey="expected"
-                    stroke="#82ca9d"
-                    fillOpacity={0}
-                    fill="url(#colorPv)"
-                  />
-                  <Area
-                    type="monotone"
-                    dataKey="amt"
-                    stroke="#82ca9d"
-                    fillOpacity={1}
-                    fill="url(#colorPv)"
-                  />
-                </AreaChart>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <Tooltip />
+                    <Area
+                      type="monotone"
+                      dataKey="achieved"
+                      stroke="#8884d8"
+                      fillOpacity={1}
+                      fill="url(#colorUv)"
+                    />
+                    <Area
+                      type="monotone"
+                      dataKey="expected"
+                      stroke="#82ca9d"
+                      fillOpacity={0}
+                      fill="url(#colorPv)"
+                    />
+                    <Area
+                      type="monotone"
+                      dataKey="amt"
+                      stroke="#82ca9d"
+                      fillOpacity={1}
+                      fill="url(#colorPv)"
+                    />
+                  </AreaChart>
+                </ResponsiveContainer>
               </>
             )}
           </div>
